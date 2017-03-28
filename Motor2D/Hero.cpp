bddee22fc58b69de4eb798d0598e745b00930f35
Andreu997 <andreu12697@gmail.com>
2017-03-28 16:45:08 +0200

    Now collisions are erased after death

--- Motor2D/Hero.cpp
+++ Motor2D/Hero.cpp
@@ -40,9 +40,9 @@ bool Hero::LoadEntity()
 	{
 		game_object = new GameObject(iPoint(150, 150), App->cf->CATEGORY_PLAYER, App->cf->MASK_PLAYER, pbody_type::p_t_player, 0);
 
-		position = iPoint(378, 164);
-		idle_collision = App->collisions->AddCollider({ position.x, position.y, 20, 50}, COLLIDER_UNIT, App->collisions); // add w/h in xml file and replace the numbers by the values in the document
-		walk_collision = App->collisions->AddCollider({ position.x, position.y, 25, 54 }, COLLIDER_UNIT, App->collisions);
+		position = { 0, 0 };
+		idle_collision = App->collisions->AddCollider({ position.x, position.y, 25, 52}, COLLIDER_UNIT, App->collisions); // add w/h in xml file and replace the numbers by the values in the document
+		walk_collision = App->collisions->AddCollider({ position.x, position.y, 25, 52 }, COLLIDER_UNIT, App->collisions);
 		attack_collision = App->collisions->AddCollider({ position.x, position.y, 30, 54 }, COLLIDER_UNIT, App->collisions);
 
 		game_object->CreateCollision(COLLISION_ADJUSTMENT, 20, 54, fixture_type::f_t_null);	
@@ -74,6 +74,8 @@ bool Hero::LoadEntity()
 		offset = i_offset;
 		direction = { 0, 1 };
 		App->entity->unit_game_objects_list.push_back(game_object);
+
+		state = unit_state::unit_idle;
 	}
 	else LOG("\nERROR, no node found\n");
 	
