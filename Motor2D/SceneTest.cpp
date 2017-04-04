e283b8a1ecf1ffa63f27613050b973e0b3e1fb14
Andreu997 <andreu12697@gmail.com>
2017-04-04 12:51:08 +0200

    Create barbarian with c key

--- Motor2D/SceneTest.cpp
+++ Motor2D/SceneTest.cpp
@@ -126,8 +126,7 @@ void SceneTest::CheckUnitCreation(iPoint p)
 	if (App->input->GetKey(SDL_SCANCODE_C) == key_down && gold >= TROOP_PRICE)
 	{
 		gold -= TROOP_PRICE;
-		troop = (Hero*)App->entity->CreateEntity(hero, player);
-		fPoint pos(App->map->MapToWorld(p.x + TROOP_OFFSET, p.y).x, App->map->MapToWorld(p.x + TROOP_OFFSET, p.y).y);
-		troop->game_object->SetPos(pos);
+		Barbarian* barb = (Barbarian*)App->entity->CreateEntity(barbarian, ally);
+		barb->game_object->SetPos(fPoint(App->map->MapToWorld(p.x + TROOP_OFFSET, p.y).x, App->map->MapToWorld(p.x + TROOP_OFFSET, p.y).y));
 	}
 }
