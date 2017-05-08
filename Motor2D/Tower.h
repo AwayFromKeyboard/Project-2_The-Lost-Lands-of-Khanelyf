dfc284ff0484f70d336c28e74efb4e88fdf92906
Eric Abad <ericabadpellicer@gmail.com>
2017-05-08 22:57:28 +0200

    tower done but missing right numbers

--- Motor2D/Tower.h
+++ Motor2D/Tower.h
@@ -1,1 +1,14 @@
-#pragma once
+#ifndef _TOWER_H_
+#define _TOWER_H_
+
+#include "Building.h"
+
+class Tower : public Building
+{
+
+public:
+	Tower(entity_type _type);
+	~Tower();
+
+	bool LoadEntity(iPoint pos); // rect_number 1,2 or 3
+};
