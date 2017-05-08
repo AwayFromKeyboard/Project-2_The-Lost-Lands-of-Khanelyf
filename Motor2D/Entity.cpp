f7983da78fb8cdd9b2dcf3b50372b4372378725d
Andreu997 <andreu12697@gmail.com>
2017-05-09 00:11:58 +0200

    Revert to solve issue #74

--- Motor2D/Entity.cpp
+++ Motor2D/Entity.cpp
@@ -13,25 +13,25 @@ void Entity::LifeBar(iPoint size, iPoint offset)
 			life_rect.w = 0;
 
 		// Back bar
-		App->scene->LayerDrawQuad(rect, 30, 30, 30, 255, true, true, 8);
+		App->scene->LayerDrawQuad(rect, 30, 30, 30, 255, true, true, 10);
 
 		if (type == entity_type::enemy || type == entity_type::enemy_building)
 		{
 			if (life >= max_life * 50 / 100) // Life bar >=50%
-				App->scene->LayerDrawQuad(life_rect, 51, 51, 255, 255, true, true, 9);
+				App->scene->LayerDrawQuad(life_rect, 51, 51, 255, 255, true, true, 11);
 			else if (life_rect.w >= rect.w * 25 / 100) // Life bar >=25%
-				App->scene->LayerDrawQuad(life_rect, 127, 0, 255, 255, true, true, 9);
+				App->scene->LayerDrawQuad(life_rect, 127, 0, 255, 255, true, true, 11);
 			else // Life bar >0%
-				App->scene->LayerDrawQuad(life_rect, 102, 0, 0, 255, true, true, 9);
+				App->scene->LayerDrawQuad(life_rect, 102, 0, 0, 255, true, true, 11);
 		}
 		else
 		{
 			if (life >= max_life * 50 / 100) // Life bar >=50%
-				App->scene->LayerDrawQuad(life_rect, 0, 255, 0, 255, true, true, 9);
+				App->scene->LayerDrawQuad(life_rect, 0, 255, 0, 255, true, true, 11);
 			else if (life_rect.w >= rect.w * 25 / 100) // Life bar >=25%
-				App->scene->LayerDrawQuad(life_rect, 255, 164, 32, 255, true, true, 9);
+				App->scene->LayerDrawQuad(life_rect, 255, 164, 32, 255, true, true, 11);
 			else // Life bar >0%
-				App->scene->LayerDrawQuad(life_rect, 255, 0, 0, 255, true, true, 9);
+				App->scene->LayerDrawQuad(life_rect, 255, 0, 0, 255, true, true, 11);
 		}
 		
 	}
